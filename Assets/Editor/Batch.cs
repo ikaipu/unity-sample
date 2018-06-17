dc1ef9ea0f844bd0fd2230a34e067a8ce31ec85c
ikaipu <kabikabitznz@gmail.com>
2018-06-17 12:57:37 +0000

    initial commit

---
+++ Assets/Editor/Batch.cs
@@ -0,0 +1,11 @@
+using UnityEditor;
+namespace Editor
+{
+    public class Batch
+    {
+        public static void OpenProject()
+        {
+            EditorApplication.ExecuteMenuItem("Assets/Open C# Project");         
+        }
+    }
+}
